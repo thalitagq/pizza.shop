a5a18e667cbfd17cbf2b2e0a0b1a13556b14db9f
thalita <thalita@Ubuntu.myguest.virtualbox.org>
2024-02-10 13:47:44 -0300

    add updated profile and signout features

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,57 +1,80 @@
 import { DropdownMenu, DropdownMenuItem, DropdownMenuContent, DropdownMenuTrigger, DropdownMenuLabel, DropdownMenuSeparator } from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
 import { Building, ChevronDown, LogOut } from "lucide-react";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { getProfile } from "@/api/get-profile";
 import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 import { Skeleton } from "./ui/skeleton";
+import { Dialog, DialogTrigger } from "./ui/dialog";
+import StoreProfileDialog from "./store-profile-dialog";
+import { signOut } from "@/api/sign-out";
+import { useNavigate } from "react-router-dom";
 
 export default function AccountMenu() {
+  const navigate = useNavigate()
+
   const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ['profile'],
     queryFn: getProfile,
+    staleTime: Infinity,
   })
 
   const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } = useQuery({
     queryKey: ['managed-restaurant'],
     queryFn: getManagedRestaurant,
+    staleTime: Infinity,
+  })
+
+  const { mutateAsync: signOutFn, isPending: isSinningOut } = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      navigate('/sign-in', { replace: true })
+    }
   })
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="flex items-center gap-2 select-none">
-          {isLoadingManagedRestaurant ? 
-            (<Skeleton className="h-4 w-40"/>) : managedRestaurant?.name
-          }
-          <ChevronDown className="w-4 h-4"/>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-56">
-        <DropdownMenuLabel className="flex flex-col">
-          {isLoadingProfile ?
-            <div className="space-y-1.5">
-              <Skeleton className="h-4 w-32" />
-              <Skeleton className="h-3 w-24" />
-            </div> : 
-            (
-              <>
-                <span>{profile?.name}</span>
-                <span className="text-xs font-normal text-muted-foreground">{profile?.email}</span>
-              </>
-            )
-          }
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator/>
-        <DropdownMenuItem>
-          <Building className="w-4 h-4 mr-2"/>
-          <span>Perfil da loja</span>
-        </DropdownMenuItem>
-        <DropdownMenuItem className="text-orange-500 dark:text-orange-400">
-          <LogOut className="w-4 h-4 mr-2" />
-          <span>Sair</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" className="flex items-center gap-2 select-none">
+            {isLoadingManagedRestaurant ?
+              (<Skeleton className="h-4 w-40" />) : managedRestaurant?.name
+            }
+            <ChevronDown className="w-4 h-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-56">
+          <DropdownMenuLabel className="flex flex-col">
+            {isLoadingProfile ?
+              <div className="space-y-1.5">
+                <Skeleton className="h-4 w-32" />
+                <Skeleton className="h-3 w-24" />
+              </div> :
+              (
+                <>
+                  <span>{profile?.name}</span>
+                  <span className="text-xs font-normal text-muted-foreground">{profile?.email}</span>
+                </>
+              )
+            }
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DialogTrigger asChild>
+            <DropdownMenuItem>
+              <Building className="w-4 h-4 mr-2" />
+              <span>Perfil da loja</span>
+            </DropdownMenuItem>
+          </DialogTrigger>
+          <DropdownMenuItem asChild className="text-orange-500 dark:text-orange-400" disabled={isSinningOut}>
+            <button className="w-full" onClick={() => signOutFn()}>
+              <LogOut className="w-4 h-4 mr-2" />
+              <span>Sair</span>
+            </button>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <StoreProfileDialog/>
+    </Dialog>
   )
 }
---
+++ src/components/store-profile-dialog.tsx
@@ -0,0 +1,113 @@
+import { DialogDescription } from "@radix-ui/react-dialog";
+import { DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "./ui/dialog";
+import { Button } from "./ui/button";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { GetManagedRestaurantResponse, getManagedRestaurant } from "@/api/get-managed-restaurant";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod";
+import { updateProfile } from "@/api/update-profile";
+import { toast } from "sonner";
+
+const storeProfileSchema = z.object({
+  name: z.string().min(1),
+  description: z.string().nullable()
+})
+
+type StoreProfileSchema = z.infer<typeof storeProfileSchema>
+
+export default function StoreProfileDialog() {
+  const queryClient = useQueryClient()
+  
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: getManagedRestaurant,
+    staleTime: Infinity,
+  })
+
+  const { 
+    register,
+    handleSubmit,
+    formState: { isSubmitting }
+  } = useForm<StoreProfileSchema>({
+    resolver: zodResolver(storeProfileSchema),
+    values: {
+      name: managedRestaurant?.name ?? '',
+      description: managedRestaurant?.description ?? ''
+    }
+  })
+
+  function updateManagedRestaurantCache({name, description}: StoreProfileSchema){
+    const cached = queryClient.getQueryData<GetManagedRestaurantResponse>(['managed-restaurant'])
+
+    if (cached) {
+      queryClient.setQueryData<GetManagedRestaurantResponse>(['managed-restaurant'], {
+        ...cached,
+        name,
+        description
+      })
+    }
+
+    return { cached }
+  }
+
+  const { mutateAsync: updateProfileFn } = useMutation({
+    mutationFn: updateProfile,
+    onMutate({ name, description }){
+      const { cached } = updateManagedRestaurantCache({ name, description })
+      
+      return { previousProfile: cached }
+    },
+    onError(_, __, context){
+      if (context?.previousProfile) {
+        updateManagedRestaurantCache(context.previousProfile)
+      }
+    }
+  })
+
+  async function handleUpdateProfile(data: StoreProfileSchema) {
+    try {
+      await updateProfileFn({
+        name: data.name,
+        description: data.description
+      })
+
+      toast.success('Perfil atualizado com sucesso!')
+    } catch (error) {
+      toast.error('Falha ao atualizar o perfil, tente novamente!')
+    }
+  } 
+
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Perfil da loja</DialogTitle>
+        <DialogDescription>
+          Informaçõs do seu estabelecimento visíveis ao seu cliente
+        </DialogDescription>
+      </DialogHeader>
+      <form onSubmit={handleSubmit(handleUpdateProfile)}>
+        <div className="space-y-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="name">Nome</Label>
+            <Input className="col-span-3" type="text" id="name" {...register('name')}/>
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="description">Descrição</Label>
+            <Textarea className="col-span-3" id="description" {...register('description')}/>
+          </div>
+        </div>
+        
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="ghost" type="button">Cancelar</Button>
+          </DialogClose>
+          <Button type="submit" variant="success" disabled={isSubmitting}>Salvar</Button>
+        </DialogFooter>
+      </form>
+    </DialogContent>
+  )
+}
