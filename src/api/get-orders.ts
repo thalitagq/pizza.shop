4d392e508a7f4ba603b960adf0c43bead9d42e57
thalita <thalita@Ubuntu.myguest.virtualbox.org>
2024-02-10 18:42:31 -0300

    add filters feature

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -2,6 +2,9 @@ import { api } from "@/lib/axios";
 
 interface GetOrdersquery{
   pageIndex?: number | null
+  orderId?: string | null
+  customerName?: string | null
+  status?: string | null
 }
 
 interface GetOrdersResponse{
@@ -19,10 +22,13 @@ interface GetOrdersResponse{
   }
 }
 
-export async function getOrders({ pageIndex }: GetOrdersquery){
+export async function getOrders({ pageIndex, customerName, orderId, status }: GetOrdersquery){
   const response = await api.get<GetOrdersResponse>('/orders', {
     params: {
-      pageIndex
+      pageIndex,
+      orderId,
+      customerName,
+      status
     }
   })
 
